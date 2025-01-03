6bb04553e81a2b14e8800d48ac78aa02e81cbe7c
Carlos Alves <110472397+CarlosHxH@users.noreply.github.com>
2025-01-03 13:37:36 -0400

    Fiz erro builder

--- src/app/dashboard/inspection/InspectionTable.tsx
+++ src/app/dashboard/inspection/InspectionTable.tsx
@@ -4,7 +4,7 @@ import {
   TableRow, Paper, IconButton, TablePagination, useTheme, useMediaQuery
 } from "@mui/material";
 import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
-import { formatDate, stringToDate } from "@/lib/ultils";
+import { formatDate } from "@/lib/ultils";
 import { InspectionType } from "@/lib/formDataTypes";
 
 interface InspectionTableProps {
