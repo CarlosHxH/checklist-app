c927f4fb296e81d321e68945bdb139b4e2feae82
Carlos Alves <110472397+CarlosHxH@users.noreply.github.com>
2025-01-08 15:39:16 -0400

    Fix mid

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import withAuth from "next-auth/middleware";
 
-const publicRoutes = ['/api/auth/signin', '/auth/signin']
-
 export default withAuth(async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  const isUnprotectedRoute = publicRoutes.includes(req.nextUrl.pathname)
-  // Verifique se o token existe ou se a rota é publica
-  if (!isUnprotectedRoute || !token) {
-    // Redirecione para a página de login se não estiver autenticado
-    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
-  }
-
   const isDashboardRoute = req.nextUrl.pathname.startsWith("/dashboard");
   const isAdmin = token && token.role === "ADMIN";
-  // Verifica se o acesso admin
+  
   if (isDashboardRoute && !isAdmin) {
     // Redireciona para a página de login se não for admin tentando acessar /dashboard
     return NextResponse.redirect(new URL("/api/auth/signin", req.url));
   }
 
+  // Verifique se o token existe
+  if (!token) {
+    // Redirecione para a página de login se não estiver autenticado
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  }
   // Se o token existir, continue com a requisição
   return NextResponse.next();
 },
