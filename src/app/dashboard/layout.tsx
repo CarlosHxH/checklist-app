3ce3b8a1e91ea533abf543c7c5c000cd0928b515
CarlosHxH <carlosdaniel.prog@gmail.com>
2025-08-19 18:28:06 -0400

    Orders table, creating...

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { PageContainer } from '@toolpad/core/PageContainer';
 import { Navigation } from '@toolpad/core/AppProvider';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -14,6 +13,7 @@ import { redirect } from 'next/navigation';
 import KeyIcon from '@mui/icons-material/Key';
 import MapIcon from '@mui/icons-material/Map';
 import RoomIcon from '@mui/icons-material/Room';
+import NoCrashIcon from '@mui/icons-material/NoCrash';
 
 // Move navigation configuration outside component to prevent recreation
 const createNavigation = (): Navigation => [
@@ -25,15 +25,20 @@ const createNavigation = (): Navigation => [
     segment: 'dashboard/keys',
     title: 'Chaves',
     icon: <KeyIcon />,
-  }, { kind: 'header', title: 'Inspeções' },{
+  }, { kind: 'header', title: 'Inspeções' }, {
     segment: 'dashboard/inspecao',
     title: 'Inspeções',
     icon: <RoomIcon />,
     pattern: 'dashboard/inspecao/:id',
-  },{
+  }, {
     segment: 'dashboard/viagens',
     title: 'Viagens',
     icon: <MapIcon />,
+    pattern: 'dashboard/viagens/:id',
+  }, {
+    segment: 'dashboard/orders',
+    title: 'Ordem de Serviço',
+    icon: <NoCrashIcon />,
     pattern: 'dashboard/viagens/:id',
   }, { kind: 'header', title: 'Outros' }, {
     segment: 'dashboard/user',
@@ -64,11 +69,11 @@ export default function DashboardPagesLayout({ children }: { children: React.Rea
   if (!mounted || status === "loading") return null;
   if (!session || !["ADMIN", "USER"].some(role => session?.user.role?.includes(role))) redirect('/');
   console.log();
-  
+
   return (
     <AppProvider session={session} authentication={authentication} branding={BRANDING} navigation={navigation}>
       <DashboardLayout>
-        <PageContainer>{children}</PageContainer>
+        {children}
       </DashboardLayout>
     </AppProvider>
   );
